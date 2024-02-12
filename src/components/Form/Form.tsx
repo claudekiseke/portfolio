bb8aae95294581d7f6e1994e724755f47887cc10
claudekiseke <claudekiseke@outlook.com>
2024-02-12 20:51:46 +0000

    Form css

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -4,13 +4,13 @@ export function Form() {
 
     return (
         <div className={styles.contactForm}>
-            <h2>Get in touch</h2>
+            <h2 className={styles.contactForm__heading}>Get in touch</h2>
             <form className={styles.contactForm__form}>
                 <input type="text" name="first-name" placeholder="First Name" className={styles.contactForm__form__input} required />
                 <input type="text" name="company" placeholder="Company (optional)" className={styles.contactForm__form__input} />
                 <input type="text" name="email" placeholder="Email Address" className={styles.contactForm__form__input} required />
-                <textarea name="message" placeholder="Message" className={styles.contactForm__form__submit} required></textarea>
-                <input type="submit" value="Submit" />
+                <textarea name="message" placeholder="Message" className={styles.contactForm__form__input} required></textarea>
+                <input type="submit" value="Submit" className={styles.contactForm__form__submit} />
             </form>
         </div>
     );
