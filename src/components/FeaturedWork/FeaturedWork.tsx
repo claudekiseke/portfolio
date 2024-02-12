0bfe23cacf1a5b52711c561c7272f5082c647ed8
claudekiseke <claudekiseke@outlook.com>
2024-02-12 22:26:46 +0000

    featured image desktop jfs

--- src/components/FeaturedWork/FeaturedWork.tsx
+++ src/components/FeaturedWork/FeaturedWork.tsx
@@ -12,6 +12,12 @@ export function FeaturedWork() {
 
             <div className={styles.featured__featuredWork__caseStudy}>
                 <div className={styles.featured__featuredWork__caseStudy__featuredImage}>
+                <Image 
+                        src="https://images.ctfassets.net/odkae62k89vn/4iyjeIBILMGSH97UCRRWwg/49990620bac56d7a347e5ff09bf14b48/Jeffrey_Campbell_Current_Designs.png"
+                        alt="Frshly Squeezd"
+                        width={297}
+                        height={408}
+                    />
                     <Image 
                         src="https://images.ctfassets.net/odkae62k89vn/G8qt5dvdfzOM3N7wOTXZG/dffaed055899bbb3bb2402a3622abb21/Jeffrey_Campbell_Proposed_Designs.png"
                         alt="Frshly Squeezd"
@@ -23,7 +29,6 @@ export function FeaturedWork() {
                     <h4 className={styles.featured__featuredWork__caseStudy__brand}>Jeffrey Campbell Shoes</h4>
                     <div className={styles.featured__featuredWork__caseStudy__caseStudyDescription}>
                         <p>Combined Group Contracting (CGC) is one of the leading construction and contracting companies in the Middle East. CGC wanted to revamp its existing website since it looked outdated with poor design. Ali worked with Poixel to develop the new website on Webflow.</p>
-                        <p>The final web design was achieved by Poixel&apos;s team. Ali worked on the pixel-perfect development of the final site with a few enhancements.The website was developed with the goal of keeping it simple to use, different from other competitor websites, and making it feel more like a digital company profile. Ali Saeed also helped with the copywriting for the website, to make sure it communicated the right message throughout the site.</p>
                     </div>
                     <ul className={styles.featured__featuredWork__caseStudy__links}>
                         <li className={styles.featured__featuredWork__caseStudy__links__linkItem}>
